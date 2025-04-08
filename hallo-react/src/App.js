a5bbe55ca9204b91256bf7a91d92a00f720733ac
RenoAryaSulaeman <say4reno@gmail.com>
2025-04-08 13:12:38 +0700

    p2

---
+++ hallo-react/src/App.js
@@ -0,0 +1,23 @@
+//import Libraries
+import React from 'react';
+import Header from './Header.jsx'
+
+
+//Membuat komponen dengan class
+
+
+
+//create component
+const App = ()=>{
+  return(
+    <>
+      <h1>Hallo React</h1>
+      <h2>Nama saya Lukmanul Hakim</h2>
+      <Header />
+    </>
+  )
+}
+
+//Export Default
+export default App;
+
